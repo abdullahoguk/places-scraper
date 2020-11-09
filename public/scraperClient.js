1228fd32c1212d9498470623f04c55b30dae917f
Abdullah <abdullahoguk@gmail.com>
2020-11-09 19:17:45 +0300

    first socket io functionality on scrape

--- public/scraperClient.js
+++ public/scraperClient.js
@@ -1,24 +1,22 @@
+var searchButton = document.querySelector("button.search");
+var queryInput = document.querySelector(".form .field #query");
+var cityInput = document.querySelector(".form .field #city");
+var zoomInput = document.querySelector(".form .field #zoom");
+var limitInput = document.querySelector(".form .field #limit");
 
-async function ajax(url) {
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
-        request.addEventListener("load", function () {
-            try {
-                resolve(this.responseText);
-            } catch (error) {
-                reject(error);
-            }
-        });
-        request.open("GET", url);
-        request.send();
-        request.addEventListener("error", reject)
-    });
-}
+var scrapedItemsContainer = document.querySelector(".results .live");
+
+
+const socket = io("/googleplaces");
+
 
 /** @returns {void} */
 async function main() {
 
-    const socket = io("/googleplaces");
+    searchButton.addEventListener("click",handleSearch);
+    socket.on("scrapedItem", handleScrapedItem);
+
+
     //socket.on("connect", () => socket.emit("hello", `Hi there! I am ${window.navigator.userAgent}`));
     socket.on("connect", function(){
         socket.emit("name", {"name":"ali"});
@@ -40,16 +38,7 @@ async function main() {
         console.info(data);
     });
 
-    setTimeout(() => {
-        socket.emit("clientmessage", "Hello to everyone");
-
-    }, 2000);
-
-
-
-
-
-
+   
 
     
     const secondsElement = document.getElementById("seconds");
@@ -72,3 +61,48 @@ main();
 Object.values(cities).forEach(city => {
     document.querySelector("#city").innerHTML+=`<option value=${city.plate}>${city.name}</option>`;
 });
+
+function handleSearch(){
+    if(queryInput.value == "" || cityInput.value == 0 || zoomInput.value == 0 || limitInput.value == 0){return}
+    var searchData = {
+        "query":queryInput.value,
+        "plate":cityInput.value,
+        "zoom": zoomInput.value,
+        "maxPages":limitInput.value
+    };
+
+    socket.emit("scrape", searchData);
+}
+
+function handleScrapedItem(data){
+    var el = document.createElement("a");
+    el.classList.add("item");
+    el.href = data.mapsUrl;
+    el.innerHTML = `
+		<div class="name">${data.name}</div>
+		<div class="website">${data.website}</div>
+		<div class="phone">${data.tel}</div>
+        <div class="address">${data.address}</div>
+    `
+
+    scrapedItemsContainer.appendChild(el);
+    scrapedItemsContainer.scrollTop = scrapedItemsContainer.scrollHeight;
+}
+
+
+async function ajax(url) {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.addEventListener("load", function () {
+            try {
+                resolve(this.responseText);
+            } catch (error) {
+                reject(error);
+            }
+        });
+        request.open("GET", url);
+        request.send();
+        request.addEventListener("error", reject)
+    });
+}
+
--- server.js
+++ server.js
@@ -1,134 +1,54 @@
-
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const { scrape } = require("./scraper.js");
 
 const SERVER_PORT = process.env.PORT || 3000;
 
-let nextVisitorNumber = 1;
-let onlineClients = new Set();
 let onlineClientsCustomUrl = new Set();
 
+// create a new express app
+const app = express();
+// create http server and wrap the express app
+const server = http.createServer(app);
+// bind socket.io to that server
+const io = socketio(server);
 
+// important! must listen from `server`, not `app`, otherwise socket.io won't function correctly
+server.listen(SERVER_PORT, () =>
+	console.info(`Listening on port ${SERVER_PORT}.`)
+);
+
+// serve static files from a given folder
+app.use(express.static("public"));
+
+//google places scraper socket
+var psSocket = io.of("/googleplaces");
+
+app.get("/googleplaces", (req, res) => {
+	res.sendFile("./public/scraper.html", { root: __dirname });
+
+	// will fire for every new websocket connection
+	psSocket.on("connection", psSocketConnection);
+});
+
+//on google places scraper socket client connection
 function psSocketConnection(socket) {
-    var username = "";
-    console.info(`Socket ${socket.id} has connected.`);
-    onlineClientsCustomUrl.add(socket.id);
+	var username = "";
+	console.info(`Socket ${socket.id} has connected.`);
+	onlineClientsCustomUrl.add(socket.id);
 
-    socket.on("disconnect", () => {
-        onlineClientsCustomUrl.delete(socket.id);
-        console.info(`Socket ${socket.id} has disconnected.`);
+	socket.on("disconnect", () => {
+		onlineClientsCustomUrl.delete(socket.id);
+		console.info(`Socket ${socket.id} has disconnected.`);
     });
-
     
-    // echoes on the terminal every "hello" message this socket sends
-    socket.on("hello", helloMsg => console.info(`Socket ${socket.id} says: "${helloMsg}"`));
-
-    // will send a message only to this socket (different than using `io.emit()`, which would broadcast it)
-    socket.emit("welcome", `Welcome! You are visitor number ${nextVisitorNumber++}`);
-
-
-    socket.on("name", function(data) {
-        username=data.name;
-        socket.emit("inform", {"name":data.name, "id": socket.id });
-        socket.broadcast.emit("notify", {name:username, id: socket.id, joined:true });
-    });
+    socket.on("scrape", (data) => {
+        console.info(`Socket ${socket.id} arama isteği yolladı.`);
+        scrape(socket, data.query, data.plate, data.zoom, data.maxPages);
+	});
 
 
-    //send a message to everyone
-    this.emit("message", "a person joined");
-
-    socket.on("clientmessage", data =>{
-        socket.broadcast.emit("clientmessage", `Socket ${socket.id} says: ${data}`);
-    }); 
+	//send a message to everyone
+	//this.emit("message", "a person joined");
 }
-
-
-
-    // create a new express app
-    const app = express();
-    // create http server and wrap the express app
-    const server = http.createServer(app);
-    // bind socket.io to that server
-    const io = socketio(server);
-    let secondsSinceServerStarted = 0;
-    
-    //google places scraper socket
-    var psSocket = io.of("/googleplaces")
-
-    app.get("/googleplaces", (req, res) => {
-        res.sendFile('./public/scraper.html', { root: __dirname })
-        
-        // will fire for every new websocket connection
-        psSocket.on("connection", psSocketConnection);
-
-        // will send one message per second to all its clients
-   
-    });
-
-    // example on how to serve static files from a given folder
-    app.use(express.static("public"));
-
-    
-
-    // important! must listen from `server`, not `app`, otherwise socket.io won't function correctly
-    server.listen(SERVER_PORT, () => console.info(`Listening on port ${SERVER_PORT}.`));
-    io.of("/").on("connection", rootSocketConnection);
-
-
-
-    setInterval(() => {
-        secondsSinceServerStarted++;
-
-        psSocket.emit("seconds", secondsSinceServerStarted);
-        psSocket.emit("online", onlineClientsCustomUrl.size);
-        psSocket.emit("message", "to scraper ns");
-
-        io.of("/").emit("seconds", secondsSinceServerStarted);
-
-        io.of("/").emit("online", onlineClients.size);
-
-        io.of("/").emit("message", "to root");
-
-
-    }, 1000);
-
-
-
-function generateRandomNumber() {
-    return (Math.floor(Math.random() * 1000)).toString();
-}
-
-function rootSocketConnection(socket) {
-    var username = "";
-    console.info(`Socket ${socket.id} has connected.`);
-    onlineClients.add(socket.id);
-
-    socket.on("disconnect", () => {
-        onlineClients.delete(socket.id);
-        console.info(`Socket ${socket.id} has disconnected.`);
-    });
-
-    
-    // echoes on the terminal every "hello" message this socket sends
-    socket.on("hello", helloMsg => console.info(`Socket ${socket.id} says: "${helloMsg}"`));
-
-    // will send a message only to this socket (different than using `io.emit()`, which would broadcast it)
-    socket.emit("welcome", `Welcome! You are visitor number ${nextVisitorNumber++}`);
-
-
-    socket.on("name", function(data) {
-        username=data.name;
-        socket.emit("inform", {"name":data.name, "id": socket.id });
-        socket.broadcast.emit("notify", {name:username, id: socket.id, joined:true });
-    });
-
-
-    //send a message to everyone
-    this.emit("message", "a person joined");
-
-    socket.on("clientmessage", data =>{
-        socket.broadcast.emit("clientmessage", `Socket ${socket.id} says: ${data}`);
-    }); 
-}
-
